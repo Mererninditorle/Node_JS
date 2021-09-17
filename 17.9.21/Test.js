1c239162241075017bec1fd92a3a8db1a21879ff
= <ivanshapovalov787@gmail.com>
2021-09-17 13:04:23 +0300

    Test

--- 17.9.21/Test.js
+++ 17.9.21/Test.js
@@ -2,13 +2,12 @@ const fs = require("fs");
 let tab = fs.readFileSync("ppl.csv", "utf-8");
 // let arr = [...tab];
 let arr = tab.split(";");
-for (var in arr) {
-    let example = arr.slice([i], 4);
-}
-
+// for (var in arr) {
+//     let exampe = arr.slice([i], 4);
+//     console.log(example[i]);
+// }
+let example = arr.slice(0, 4);
 console.log(example);
-
-
 // 2)  Наполнить массив людей на основании файла (с использованием класса);
 // 3)  Показать имя для документов в порядке приближения даты рождения
 //     3v-lite Вывести имя для документов всех представителей одного пола
