56ebad0f6a398d0b634cc0f12bca3136fd68cf5d
= <ivanshapovalov787@gmail.com>
2021-09-29 17:09:43 +0300

    Image

--- 29.9.21/class.js
+++ 29.9.21/class.js
@@ -1,1 +1,13 @@
-console.log("brunya");
+const http = require("http");  // Hyper Text Transport Protocol
+const fs = require("fs");
+const PORT = 8888;
+const server = http.createServer((req, res) => {
+    // res.write("<h3>Sponge Bob</h3><img src=\"https://i.imgur.com/rnPzj94.jpg\" type=\"image/jpeg\">");
+    let img = fs.readFileSync("./Heli.jpg");
+    console.log(img);
+    res.write(img);
+    res.end();
+});
+server.listen(PORT, err => {
+    console.log(`Server is starting on port: ${PORT}`);
+});
