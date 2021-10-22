0b3e2d775c4148e217b8fbd24f3b0ca6786a681e
= <ivanshapovalov787@gmail.com>
2021-10-22 13:11:46 +0300

    Server

--- 20.10.21/public/scripts/front.js
+++ 20.10.21/public/scripts/front.js
@@ -1,15 +1,56 @@
 let data = [
     {name: "Paint", src: "/images/a_1.jpg"}, 
-    {name: "Клякса", src: "/images/a2_.jpg"},
-    {name: "Stain", src: "/images/a3_.jpg"},
-    {name: "Macos Wallpaper", src: "/images/a4_.jpg"},
-    {name: "Circle", src: "/images/a5_.jpg"},
-    {name: "Hypnotic", src: "/images/a6_.jpg"},
+    {name: "Клякса", src: "/images/a_2.jpg"},
+    {name: "Stain", src: "/images/a_3.jpg"},
+    {name: "Macos Wallpaper", src: "/images/a_4.jpg"},
+    {name: "Circle", src: "/images/a_5.jpg"},
+    {name: "Hypnotic", src: "/images/a_6.jpg"},
+    // {name: "Guest", src: "/images/guest.png"},
 ];
 
 const outer = document.querySelector(".container");
 data.forEach(el => {
     outer.innerHTML += `
-        <div class = "card">${el.name}</div>
+        <div class = "card" 
+            data-src = "${el.src}"; 
+            data-name = ${el.name}
+        >${el.name}</div>
     `;
+});
+
+
+
+let other = document.createElement("div");
+other.className = "card";
+other.style.height = "100px";
+other.setAttribute("data-src", "/images/guest.png");
+let inp = document.createElement("input");
+other.append(inp);
+outer.append(other);
+inp.addEventListener("click", e => {
+    e.stopPropagation();
+});
+inp.addEventListener("input", e => {
+    e.target.parentElement.setAttribute("data-name", e.target.value);
+})
+
+
+/* 
+* При нажатии на карточку отправляться на страницу с соответсвующим изображением (img/<name>)
+*/ 
+
+outer.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", e => {
+        // location.reload - перезагрузить страницу
+        // location.replace - отправить по другому адресу
+
+        /* 
+        let name = e.target.getAttribute("data-name")};
+        let src = e.target.getAttribute("data-src");
+        let addres = `personal?name=${name} &img =${src}`;
+        location.replace.addres
+        */ 
+
+        location.replace(`/personal?name=${e.target.getAttribute("data-name")}&img=${e.target.getAttribute("data-src")}`); // отправить по другому адресу
+    });
 })
--- 20.10.21/server/routes/server.js
+++ 20.10.21/server/routes/server.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-// router.get("/", (rq,rs) => {
-//     rs.
-// })
+router.get("/personal", (rq,rs) => {
+    const name = rq.query.name;
+    const img = rq.query.img;
+    console.log(rq.query); // - получение данных от клиента, переданных при помощи get-запроса (в адресной строке браузера)
+    rs.send(`
+     <div>
+     <img src="${img}" alt= "${name}" >
+     <h1>${name}</h1>
+     </div>
+    `);
+});
 
-module.exports = router
+module.exports = router;
